e5168bd07da39194222c16c1090166aa8c7ed80c
dakom35 <alfred.lalanne@gmail.com>
2023-12-09 18:15:28 +0100

    Player's sprites & textures added

--- Entities/Player.h
+++ Entities/Player.h
@@ -1,15 +1,21 @@
 #ifndef PLAYER_H
 #define PLAYER_H
+#define NB_TEXTURES_PLAYER 16 // 16 sprites necessary to display every moving positions (going down, up, ...) 
+                                // TODO : remove this is C style and ugly
 
 #include "Entity.h"
 
 class Player : public Entity {
 private:
     // Add any additional member variables specific to the Player class here
+    std::array<sf::Sprite, NB_TEXTURES_PLAYER> mvmtSprites ;  
+    std::array<sf::Texture,NB_TEXTURES_PLAYER> mvmtTextures ;
+
 
 public:
     Player();
     ~Player();
+    int initPlayerSprites();
 
     // Add any additional member functions specific to the Player class here
 };
