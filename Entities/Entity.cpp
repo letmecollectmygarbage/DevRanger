fa39e6f1267addca45c3b085a1b794a7babf9784
dakom35 <alfred.lalanne@gmail.com>
2023-12-09 12:40:50 +0100

    added Entity class

---
+++ Entities/Entity.cpp
@@ -0,0 +1,56 @@
+#ifndef ENTITY_H
+#define ENTITY_H
+
+#include "Entity.h"
+
+
+
+
+#endif
+
+Entity::Entity()
+{
+    this->shape.setSize(sf::Vector2f(50.f,50.f));
+    this->movementSpeed = 300.f ;
+}
+
+Entity::~Entity()
+{
+}
+
+void Entity::update(const float &dt)
+{
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Q)) // left 
+    {
+        this->move(dt,-1.f,0.f);
+    }
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Z)) // up
+    {
+        this->move(dt,0.f,-1.f);
+    }
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::D)) // right
+    {
+        this->move(dt,1.f,0.f);
+    } 
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::S)) // down
+    {
+        this->move(dt,0.f,1.f);
+    }
+}
+
+void Entity::render(sf::RenderTarget* target)
+{
+    target->draw(this->shape);
+}
+
+void Entity::move(const float& dt, const float dir_x, const float dir_y)
+/*
+*       -1 <= dir_x <= 1
+*       -1 <= dir_y <= 1
+*
+*/
+
+{
+    this->shape.move(dir_x*this->movementSpeed*dt,dir_y*this->movementSpeed*dt);
+    
+}
---
+++ Entities/Entity.h
@@ -0,0 +1,36 @@
+
+
+#include <iostream>
+#include <cstdlib> // for random enemy positions
+#include <ctime> // for random enemy positions
+#include <chrono> // for FPS measurement
+#include <stack>
+#include <vector>
+#include <map>
+
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+
+class Entity
+{
+    private : 
+
+
+    protected : 
+    sf::RectangleShape shape ; 
+    float movementSpeed ; 
+
+
+
+
+    public:
+        Entity();
+        virtual ~Entity(); // virtual keyword means this function is intended to be derived by derived classes
+
+        // Functions 
+        virtual void update(const float& dt) ;
+        virtual void render(sf::RenderTarget* target) ;
+        virtual void move(const float& dt, const float x, const float y);
+};
