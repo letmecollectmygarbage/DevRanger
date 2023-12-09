9a3e1627ba5818105d4a1d3e526de90c7dc1f61d
dakom35 <alfred.lalanne@gmail.com>
2023-12-09 15:25:03 +0100

    Player class derived from Entity added

---
+++ Entities/Player.cpp
@@ -0,0 +1,11 @@
+#include "Player.h"
+
+Player::Player() {
+    // Initialize Player-specific properties if needed
+}
+
+Player::~Player() {
+    // Destructor for Player, if needed
+}
+
+// Implement any additional member functions for the Player class if needed
---
+++ Entities/Player.h
@@ -0,0 +1,17 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include "Entity.h"
+
+class Player : public Entity {
+private:
+    // Add any additional member variables specific to the Player class here
+
+public:
+    Player();
+    ~Player();
+
+    // Add any additional member functions specific to the Player class here
+};
+
+#endif
