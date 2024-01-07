d97fcd28787863f46017977f92959743f94f7f35
dakom35 <alfred.lalanne@gmail.com>
2024-01-07 13:43:42 +0100

    added comments

--- Entities/Player.cpp
+++ Entities/Player.cpp
@@ -15,39 +15,12 @@ Player::~Player()
 
 int Player::initPlayerSprites()
 {
-    /*
-    *   @brief :    Loads all the sprites necessary for displaying a moving player
-                        // IDLE //
-                            0 IDLE up
-                            1 IDLE right
-                            2 IDLE down
-                            3 IDLE left
-                        // going left //
-                            4 half left step
-                            5 full left step
-                            6 half right step
-                            7 full right step
-                        // going right //
-                            8 half left step
-                            9 full left step
-                            10 half right step
-                            11 full right step
-                        // going up //
-                            12 half right step
-                            13 full right step
-                            14 half left step
-                            15 full left step
-                        // going down //
-                            16 right step
-                            17 left step              
-    *   @return : -1 if at least one image file was not found
-    *              0 if all good 
-    */
-    int nbMvtIDLE = 4 ; // 28 sprites necessary to display every moving positions (going down, up, steps, ...)
-    int nbMvtUP = 6 ;
-    int nbMvtDOWN = 6 ;
-    int nbMvtLEFT = 6 ;
-    int nbMvtRIGHT = 6 ;
+
+    int nbMvtIDLE = 4 ; // "number of sprites necessary to represent the hero" IDLE in every direction
+    int nbMvtUP = 6 ; // "..." moving up
+    int nbMvtDOWN = 6 ; // "..." moving down
+    int nbMvtLEFT = 6 ; // "..." moving left
+    int nbMvtRIGHT = 6 ; // "..." moving right
 
     std::string filename,imagesFolder,imagesPlayerFolder;
     std::filesystem::path currentPath = std::filesystem::current_path();
@@ -55,25 +28,25 @@ int Player::initPlayerSprites()
     imagesPlayerFolder = "Player/Moves" ; 
     std::filesystem::path folderPath = currentPath/imagesFolder/imagesPlayerFolder; 
 
-    // LOAD EVERY NECESSARY TEXTURE FOR HERO MOVEMENT //
+    // LOAD EVERY TEXTURE NECESSARY FOR HERO MOVEMENT //
+
     for(int i = 0 ; i < nbMvtIDLE ; i++) // IDLE
     {
         filename = "IDLE_" + std::to_string(i+1) + ".png"; 
         if(!this->txtrIDLE[i].loadFromFile(folderPath/filename))
         {
-            std::cerr << "The image " + std::to_string(i) + " was not found \n" ;
+            std::cerr << "The image IDLE_" + std::to_string(i) + " was not found \n" ;
             std::filesystem::path currentPath = std::filesystem::current_path();
             std::cout << "Current working directory: " << currentPath.string() << std::endl;
             return -1 ; 
         }
     }
-
     for(int i = 0 ; i < nbMvtUP ; i++) // UP
     {
         filename = "UP_" + std::to_string(i+1) + ".png"; 
         if(!this->txtrUP[i].loadFromFile(folderPath/filename))
         {
-            std::cerr << "The image " + std::to_string(i) + " was not found \n" ;
+            std::cerr << "The image UP_" + std::to_string(i) + " was not found \n" ;
             std::filesystem::path currentPath = std::filesystem::current_path();
             std::cout << "Current working directory: " << currentPath.string() << std::endl;
             return -1 ; 
@@ -85,7 +58,7 @@ int Player::initPlayerSprites()
         filename = "LEFT_" + std::to_string(i+1) + ".png"; 
         if(!this->txtrLEFT[i].loadFromFile(folderPath/filename))
         {
-            std::cerr << "The image " + std::to_string(i) + " was not found \n" ;
+            std::cerr << "The image LEFT_" + std::to_string(i) + " was not found \n" ;
             std::filesystem::path currentPath = std::filesystem::current_path();
             std::cout << "Current working directory: " << currentPath.string() << std::endl;
             return -1 ; 
@@ -97,7 +70,7 @@ int Player::initPlayerSprites()
         filename = "RIGHT_" + std::to_string(i+1) + ".png"; // be careful to name the pictures in the Images/Moves folder as defined below
         if(!this->txtrRIGHT[i].loadFromFile(folderPath/filename))
         {
-            std::cerr << "The image " + std::to_string(i) + " was not found \n" ;
+            std::cerr << "The image RIGHT_" + std::to_string(i) + " was not found \n" ;
             std::filesystem::path currentPath = std::filesystem::current_path();
             std::cout << "Current working directory: " << currentPath.string() << std::endl;
             return -1 ; 
@@ -109,7 +82,7 @@ int Player::initPlayerSprites()
         filename = "DOWN_" + std::to_string(i+1) + ".png"; // be careful to name the pictures in the Images/Moves folder as defined below
         if(!this->txtrDOWN[i].loadFromFile(folderPath/filename))
         {
-            std::cerr << "The image " + std::to_string(i) + " was not found \n" ;
+            std::cerr << "The image DOWN_" + std::to_string(i) + " was not found \n" ;
             std::filesystem::path currentPath = std::filesystem::current_path();
             std::cout << "Current working directory: " << currentPath.string() << std::endl;
             return -1 ; 
@@ -140,8 +113,8 @@ int Player::initPlayerSprites()
     }
 
     // DEFINE DEFAULT POSITION FOR HERO
-    playerSprite = sprtIDLE[0] ;
-    std::cout << "Reached end of initPlayerSprites()" << "\n" ; 
+    playerSprite = sprtIDLE[0] ; // IDLE facing down
+    std::cerr << "[INFO] initPlayerSprites() achieved" << "\n" ; 
     return 0 ;
 }
 
@@ -155,15 +128,18 @@ void Player::render(sf::RenderTarget* target)
 void Player::move(const float& dt, const float dir_x, const float dir_y)
 {
     /*
-    *       -1 <= dir_x <= 1
-    *       -1 <= dir_y <= 1
+    *   This is the function that makes the hero move. 
+    *   Arguments : dir_x and dir_y values are keyboard inputs
+    *   For correct behaviour only use this functions with   
+    *   dir_x belonging to [-1;1]
+    *   dir_y belonging to [-1;1]
     */
-   static char lastMvmt = 'I' ; // initially IDLE
-   static int cyclePos = 0 ; // position in the current movement cycle (0->5)
-   static int repeat = 4 ; // number of times each sprite is repeated, used to slow down the display
-   static int i = 0 ; 
+    static char lastMvmt = 'I' ; // initiated as IDLE down
+    static int cyclePos = 0 ; // position in the current movement cycle (0->5)
+    static int i = 0 ; 
+    int repeat = 16 ; // number of times each sprite is repeated, used to slow down the 6 frame movement cycle
     if(dir_x == 0.f && dir_y == -1.f) // player going UP
-   {
+    {
         if(lastMvmt == 'U') // if already walking UP
         { 
             if(i < repeat)
@@ -178,9 +154,8 @@ void Player::move(const float& dt, const float dir_x, const float dir_y)
                 cyclePos++;
                 if(cyclePos >= 6) cyclePos = 0 ; // reset counter to not go outside the bounds of sprtUP
             }
-
         }
-        else// was not walking UP before
+        else // was not walking UP before
         {
             cyclePos = 0 ; // reset counter 
             sprtUP[cyclePos].setPosition(this->pos);
@@ -188,9 +163,9 @@ void Player::move(const float& dt, const float dir_x, const float dir_y)
             cyclePos++;
             lastMvmt = 'U';
         }
-   }
+    }
     else if(dir_x == -1.f && dir_y == 0.f) // player going LEFT
-   {
+    {
         if(lastMvmt == 'L') // if already walking LEFT
         { 
             if(i < repeat)
@@ -230,7 +205,6 @@ void Player::move(const float& dt, const float dir_x, const float dir_y)
                 cyclePos++;
                 if(cyclePos >= 6) cyclePos = 0 ;
             }
- 
         }
         else// was not walking RIGHT before
         {
@@ -256,7 +230,6 @@ void Player::move(const float& dt, const float dir_x, const float dir_y)
                 cyclePos++;
                 if(cyclePos >= 6) cyclePos = 0 ;
             }
- 
         }
         else// was not walking DOWN before
         {
@@ -290,7 +263,12 @@ void Player::move(const float& dt, const float dir_x, const float dir_y)
             this->playerSprite= sprtIDLE[3];
         }
    }
-   if(dir_x==0 || dir_y==0) // ensure hero cannot move in diagonal
+//    else // player is trying to move diagonally : block him in IDLE down
+//    {
+//     sprtIDLE[0].setPosition(this->pos);
+//     this->playerSprite= sprtIDLE[0];
+//    }
+   if ( (dir_x == 0.f) ^ (dir_y == 0.f) ) // ensure hero cannot move in diagonal (^ = XOR)
    {
         this->playerSprite.move(dir_x*this->movementSpeed*dt,dir_y*this->movementSpeed*dt);
         this->pos = playerSprite.getPosition(); // update current position of player
