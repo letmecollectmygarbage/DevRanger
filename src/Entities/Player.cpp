3da0edfd26a7d2b2575980f95d19b78b6f3949d7
dakom35 <123820119+dakom35@users.noreply.github.com>
2024-08-02 01:18:18 +0200

    - added class attributes : 1)imagesPerMovement 2) numberOfDifferentMovements 3) playerWidth 4) playerHeight 5) sf::Vector2f size 6) float initialX,initialY 7) sf::Vector2f initialPos 8) renamed player sprite as sprite 9) renamed lastMvmt to lastMovement - removed some useless 'this->'

--- src/Entities/Player.cpp
+++ src/Entities/Player.cpp
@@ -2,12 +2,10 @@
 
 Player::Player() 
 {
-    
-    sf::Vector2f initialSize = sf::Vector2f(50.f,50.f);
-    this->shape.setSize(initialSize); // 
-    this->movementSpeed = 200.f ;
+    shape.setSize(size); 
+    movementSpeed = 200.f ;
     initPlayerSprites();
-    this->pos = {0.f,0.f};
+    
 }
 
 Player::~Player() 
@@ -27,7 +25,7 @@ int Player::initPlayerSprites(){
     imagesFolder = "ressources/Images/" ;
     imagesPlayerFolder = "Player/Moves/" ; 
     std::string folderPath = currentPath+imagesFolder+imagesPlayerFolder; 
-    this->lastMvmt = "IDLE" ; // initiated as IDLE down
+    lastMovement = "IDLE" ; // initiated as IDLE down
     // LOAD EVERY TEXTURE NECESSARY FOR HERO MOVEMENT //
     // IDLE:
     
@@ -39,30 +37,30 @@ int Player::initPlayerSprites(){
         else if(j==2){mvmtID = "DOWN";}
         else if(j==3){mvmtID = "LEFT";}
         else if(j==4){mvmtID = "RIGHT";}
+        // Initialize the vectors in the maps
+        textureMap[mvmtID] = std::vector<sf::Texture>(imagesPerMovement);
+        spriteMap[mvmtID] = std::vector<sf::Sprite>(imagesPerMovement);
         // For the number of sprites each movement has
         for(int i = 0 ; i < imagesPerMovement ; i++){
             filename = mvmtID + +"_"+std::to_string(i+1) + ".png" ; 
-            if(!this->textureMap[mvmtID][i].loadFromFile(folderPath+filename)){
+            if(!textureMap[mvmtID][i].loadFromFile(folderPath+filename)){
                 std::cerr << "The image"+mvmtID+"_"+ std::to_string(i) + " was not found \n" ;
                 return -1 ; 
             }
-            this->spriteMap[mvmtID][i].setTexture(this->textureMap[mvmtID][i]);
+            spriteMap[mvmtID][i].setTexture(textureMap[mvmtID][i]);
         }
     }
 
-
-
-
     // make hero start IDLE facing the user
-    playerSprite = this->spriteMap["IDLE"][0] ; // IDLE facing down
+    sprite = spriteMap["IDLE"][0] ; // IDLE facing down
     std::cerr << "[INFO] Player::initPlayerSprites() achieved" << "\n" ; 
     return 0 ;
 }
 
-// Implement any additional member functions for the Player class if needed
+
 
 void Player::render(sf::RenderTarget* target){
-    target->draw(this->playerSprite);
+    target->draw(this->sprite);
 }
 
 /*
@@ -77,33 +75,33 @@ void Player::move(const float& dt, const float dir_x, const float dir_y){
 
     nextSprite(dir_x, dir_y);
 
-    this->playerSprite.move(dir_x*this->movementSpeed*dt,dir_y*this->movementSpeed*dt);
-    this->pos = playerSprite.getPosition(); // update current position of player
+    sprite.move(dir_x*movementSpeed*dt,dir_y*movementSpeed*dt);
+    pos = sprite.getPosition(); // update current position of player
 }
 
 void Player::nextSprite(float dir_x, float dir_y){
     static int cyclePos = 0 ; // position in the current movement cycle (0->5)
     static int i = 0 ; 
     int repeat = 4 ; // number of times each sprite is repeated, used to slow down the 6 frame movement cycle
-    std::string Mvmt ; // most up to date Movement request from user
+    std::string movement ; // most up to date Movement request from user
 
     // HANDLE FRAME CYCLES //
 
-    // Determine Mvmt direction based on dir_x and dir_y (user input)
-    if(dir_x == 0.f && dir_y == -1.f){Mvmt = "UP";}
-    else if(dir_x == -1.f && dir_y == 0.f){Mvmt = "LEFT";}
-    else if(dir_x == 1.f && dir_y == 0.f){Mvmt = "RIGHT";}
-    else if(dir_x == 0.f && dir_y == 1.f){Mvmt = "DOWN";}
+    // Determine movement direction based on dir_x and dir_y (user input)
+    if(dir_x == 0.f && dir_y == -1.f){movement = "UP";}
+    else if(dir_x == -1.f && dir_y == 0.f){movement = "LEFT";}
+    else if(dir_x == 1.f && dir_y == 0.f){movement = "RIGHT";}
+    else if(dir_x == 0.f && dir_y == 1.f){movement = "DOWN";}
 
 
 
     // hero walks in the same direction as last input
-    if(this->lastMvmt == Mvmt){ 
+    if(lastMovement == movement){ 
         if(i < repeat) i++ ; // do nothing to slow down display a little
         else{ // take next sprite of same direction
             i = 0 ; 
-            this->playerSprite = spriteMap[Mvmt][cyclePos] ;
-            this->playerSprite.setPosition(this->pos);
+            sprite = spriteMap[movement][cyclePos] ;
+            sprite.setPosition(pos);
             cyclePos++;
             if(cyclePos >= 6) cyclePos = 0 ; // reset counter to not go outside the bounds of sprtUP
         }
@@ -111,11 +109,12 @@ void Player::nextSprite(float dir_x, float dir_y){
     // hero changes direction
     else{
         cyclePos = 0 ; // reset counter 
-        this->playerSprite = spriteMap[Mvmt][cyclePos] ;
-        this->playerSprite.setPosition(this->pos);
+        sprite = spriteMap[movement][cyclePos] ;
+        sprite.setPosition(pos);
         cyclePos++;
-        this->lastMvmt = Mvmt;
+        lastMovement = movement;
     }
     return;
 }
+
 
--- src/Entities/Player.h
+++ src/Entities/Player.h
@@ -6,18 +6,29 @@
 // Player class is a specialization of Entity class
 class Player : public Entity {
 private:
-
+    
     // Map to link sprite arrays to their movement directions
-    std::map<std::string, std::array<sf::Sprite, 6>> spriteMap;
+    int imagesPerMovement = 6 ; // according to what I have in Images/player/Moves
+    int numberOfDifferentMovements = 5 ; // also according to what I have in Images/player/Moves
+    std::size_t imagesPerMovementSize = 6 ;
+    std::map<std::string, std::vector<sf::Sprite>> spriteMap;
+    
 
     // Map to link texture arrays to their movement directions
-    std::map<std::string, std::array<sf::Texture, 6>> textureMap;
+    std::map<std::string, std::vector<sf::Texture>> textureMap;
 
+    float playerWidth = 50.f ; // player width
+    float playerHeight = 50.f ; // player height
+    sf::Vector2f size = sf::Vector2f(this->playerWidth,this->playerHeight); // size of the player
 
+    float initialX = 0.f ; // Initial X position
+    float initialY = 0.f ;  // Initial Y position
+    sf::Vector2f initialPos = {this->initialX,this->initialY} ; // Initial position 
     
-    sf::Sprite playerSprite ; // the player that the user can move
+    sf::Sprite sprite ; // the player that the user can move
     sf::Vector2f pos; // position of the player 
-    std::string lastMvmt;
+    std::string lastMovement;
+
 
 
 public:
