639f6401d04153420f1d59bd3e7e4521a1401895
dakom35 <123820119+dakom35@users.noreply.github.com>
2024-08-01 18:38:48 +0200

    - removed unused macro NB_TEXTURES_PLAYER - added comments

--- src/Entities/Player.h
+++ src/Entities/Player.h
@@ -1,25 +1,26 @@
 #ifndef PLAYER_H
 #define PLAYER_H
-#define NB_TEXTURES_PLAYER 28 // 28 sprites necessary to display every moving positions (going down, up, ...) 
-                                // TODO : remove this is C style and ugly and might cause runtime errors if we add sprites in cpp file without adjusting size
 
 #include "Entity.h"
 
+// Player class is a specialization of Entity class
 class Player : public Entity {
 private:
     // Add any additional member variables specific to the Player class here
+    // sprites for all directions
     std::array<sf::Sprite, 4> sprtIDLE ;  
     std::array<sf::Sprite, 6> sprtUP ; 
     std::array<sf::Sprite, 6> sprtDOWN ; 
     std::array<sf::Sprite, 6> sprtLEFT ; 
     std::array<sf::Sprite, 6> sprtRIGHT ; 
 
+    // textures for all directions
     std::array<sf::Texture, 4> txtrIDLE ;  
     std::array<sf::Texture, 6> txtrUP ; 
     std::array<sf::Texture, 6> txtrDOWN ; 
     std::array<sf::Texture, 6> txtrLEFT ; 
     std::array<sf::Texture, 6> txtrRIGHT ; 
-
+    
     sf::Sprite playerSprite ; // the player that the user can move
     sf::Vector2f pos; // position of the player 
 
