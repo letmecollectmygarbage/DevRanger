8ee387e51fcceb965eccd8005fff4ef8a0f0f7d6
dakom35 <123820119+dakom35@users.noreply.github.com>
2024-08-02 20:45:28 +0200

    typo

--- src/States/GameState.h
+++ src/States/GameState.h
@@ -8,15 +8,13 @@ class GameState :
     {
 
         private:
-            // Player used for this state. Useful if you want to control another player during the game. (cv. GTA5).
+            // Player used for this state. Useful if you want to control another player during the game. (cf. GTA5).
             Player statePlayer ; 
 
         public:
             GameState(sf::RenderWindow* window, sf::View view);
             virtual ~GameState();
 
-
-             
             void endState();
             void updateInput(const float& dt);
             void update(const float& dt);
