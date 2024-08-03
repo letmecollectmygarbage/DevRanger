c7eb2a3f4a1b29de8457c2682921661c60d5fd6a
dakom35 <123820119+dakom35@users.noreply.github.com>
2024-08-03 18:19:13 +0200

    new class to describe monsters appearance and behaviour

---
+++ src/Entities/Monster.h
@@ -0,0 +1,21 @@
+#ifndef MONSTER_H
+#define MONSTER_H
+
+#include "Entity.h"
+
+// Monster class is a specialization of Entity class
+class Monster : public Entity {
+private:
+    
+    
+
+    
+
+
+
+public:
+
+};
+
+
+#endif 
