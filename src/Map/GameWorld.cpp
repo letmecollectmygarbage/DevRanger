baaa2a1f6ae3f80770cd653df332d258ca2a5cf3
dakom35 <123820119+dakom35@users.noreply.github.com>
2024-08-24 15:50:00 +0200

    added comments

--- src/Map/GameWorld.cpp
+++ src/Map/GameWorld.cpp
@@ -23,19 +23,20 @@ void GameWorld::setUpTiles()
     bool resize = true ;
     bool isFree = false ; 
     sf::Vector2f posFirstRow = {0.f,0.f};
-    sf::Vector2f posBottomRow = {0.f,568.f};
+    sf::Vector2f posBottomRow = {0.f,568.f}; // objective height = 600px here
     std::vector<GameTile *> firstRow ; // row != column
     std::vector<GameTile *> bottomRow ; 
     for(int i = 0 ; i < gridSize.x ; i++){
         // create top wall 
-        firstRow.push_back(new GameTile(wallPath,posFirstRow,isFree,scaleFactor,resize)); // bc each tile is a 32x32px
-        bottomRow.push_back(new GameTile(wallPath,posBottomRow,isFree,scaleFactor,resize)); // 32 * gridSize.x = 800
-        
+        GameTile* topWall = new GameTile(wallPath,posFirstRow,isFree,scaleFactor,resize);
+        GameTile* bottomWall = new GameTile(wallPath,posBottomRow,isFree,scaleFactor,resize);
+        firstRow.push_back(topWall); // bc each tile is a 32x32px
+        bottomRow.push_back(bottomWall); // 32 * gridSize.x = 800
         posFirstRow.x += 32.f ; // increment by width of tile
-        posBottomRow.x = posFirstRow.x ; // symmetrical with top wall
+        posBottomRow.x = posFirstRow.x ; // symetrical with top wall
     }
 
-    firstRow[8]->setUpSprite(doorPath,scaleFactor,resize) ;  // TODO : find a way to set isExit attribute to true for this one
+    firstRow[8]->setUpSprite(doorPath,scaleFactor,resize) ; 
     tiles.push_back(firstRow); 
     tiles.push_back(bottomRow);
 }
