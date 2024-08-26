3ce4a9c91defd0195a321d781be1517a74b0a006
dakom35 <123820119+dakom35@users.noreply.github.com>
2024-08-26 15:20:20 +0200

    - removed unused attributes exitPos, gridSize

--- src/Map/GameWorld.h
+++ src/Map/GameWorld.h
@@ -6,14 +6,13 @@
 class GameWorld
 {
     private : 
-        sf::Vector2i exitPos ; 
+
         void setUpTiles();
 
 
 
     public : 
         std::vector<GameTile*> tiles ; 
-        sf::Vector2i gridSize ; // x = width , y = length 
         GameWorld();
 
 };
