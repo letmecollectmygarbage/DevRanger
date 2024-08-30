51d4d41e5680e7105c384f5fa6e05997417a7327
dakom35 <123820119+dakom35@users.noreply.github.com>
2024-08-31 00:40:45 +0200

    improved Menu class : - added background sprites - improved constructor - improved render method - made state close with escape keyboard input

--- src/States/Menu.cpp
+++ src/States/Menu.cpp
@@ -3,7 +3,36 @@
 Menu::Menu(sf::RenderWindow* window, sf::View view)
     : State(window,view)
 {
+    this->view = view ;  
     std::cout << "Entering Menu constructor \n";
+    int resX = 1920 ; 
+    int resY = 1080 ; 
+    
+
+    sf::Vector2f pos = {0.f,0.f} ;  
+    std::string textureFilename = "./ressources/Images/background_96x96.png" ;
+    sf::Sprite sprite ; 
+    if(!background_texture.loadFromFile(textureFilename)){ // loading image failed
+        std::cout << "Failed loading sprite \n";
+        return ;
+    } 
+    sf::Vector2u sizeTexture = background_texture.getSize() ;
+    int lines = resY/sizeTexture.y + 1 ; // min number of lines to fill resX px with texture
+    if(resY%sizeTexture.y==0) lines = resY/sizeTexture.y ; // lucky case
+    int cols = resX/sizeTexture.x + 1 ; // min number of cols to fill resY px with texture
+    if(resX%sizeTexture.x==0) cols = resX/sizeTexture.x ; // lucky case
+    background_texture.setSmooth(true); // blurs edges
+    for(int i = 0 ; i < lines ; i++){
+        pos.x = 0.f ; // reset
+        for(int j = 0 ; j < cols ; j++){
+            // 
+            sprite.setTexture(background_texture);
+            sprite.setPosition(pos);
+            background_sprites.push_back(sprite);
+            pos.x += sprite.getGlobalBounds().getSize().x ;
+        }
+        pos.y += sprite.getGlobalBounds().getSize().y ; 
+    }
     choices.push_back("Retry");
     choices.push_back("Exit DevRanger");
     if (!font.loadFromFile("./ressources/Fonts/arial.ttf")){
@@ -12,17 +41,15 @@ Menu::Menu(sf::RenderWindow* window, sf::View view)
     }
         // set the character size
     sf::Text choice_text ;
-    choice_text.setCharacterSize(24); // in pixels, not points!
-    choice_text.setFillColor(sf::Color::Red);
-    choice_text.setStyle(sf::Text::Bold);
-    sf::Vector2f pos_text = {200.f,200.f};
+    sf::Vector2f pos_text = {0.f,0.f};
+    int policeSize = 30 ;
     for(std::string choice : choices){
-        choice_text.setString(choice);
+        choice_text = sf::Text(choice,font,policeSize);
+        choice_text.setFillColor(sf::Color::Green);
         choice_text.setPosition(pos_text);
-        pos_text.y = pos_text.y - 50.f ; 
+        pos_text.y = pos_text.y + 50.f ; 
         choices_text.push_back(choice_text);
     }
-    
 }
 
 
@@ -36,15 +63,25 @@ void Menu::endState(){
 }
 
 void Menu::update(const float &deltaTime){
-    this->updateInput(deltaTime);  
+    this->updateInput(deltaTime); 
 }
 
 
 void Menu::render(sf::RenderTarget* target){
-    //this->statePlayer.render(target);
-    for(auto choice : choices_text){
+
+
+    // BACKGROUND DRAWING
+    for(sf::Sprite sprite : background_sprites){
+        target->draw(sprite);
+    }
+
+    // FOREGROUND DRAWING
+    for(sf::Text choice : choices_text){
         target->draw(choice);
     }
+    target->draw(test_text);
+
+
 }
 
 
@@ -53,15 +90,12 @@ void Menu::updateInput(const float& deltaTime){
     static bool btn_pushed = false ; 
     static bool btn_released = false ; 
     static int i = 0 ; 
+    this->checkForQuit();
     if(sf::Keyboard::isKeyPressed(sf::Keyboard::Z)){
         while(!sf::Keyboard::isKeyPressed(sf::Keyboard::Z)){ // wait until key is released
             btn_pushed = true ; 
         }
         btn_released = true ; 
-        i++;
-        if(i > choices.size()-1) i=0;
-        choice = choices[i];
-        std::cout << choice << "\n";
     }
 }
 
--- src/States/Menu.h
+++ src/States/Menu.h
@@ -9,11 +9,15 @@ class Menu :
 
         private:
             std::string choice ;
+            bool quit ; 
             std::vector<std::string> choices ; 
             sf::Texture selectionArrowTexture ; // texture of the arrow used to indicate user's choice
             sf::Sprite selectionArrow ; // sprite of the arrow used to indicate user's choice
             sf::Font font;
             std::vector<sf::Text> choices_text ; 
+            sf::Text test_text ; 
+            sf::Texture background_texture ; // texture used for all background sprites
+            std::vector<sf::Sprite> background_sprites ; // background sprites
 
             
         public:
