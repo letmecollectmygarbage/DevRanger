552bb122677d275f0916209618edb26978875e81
dakom35 <123820119+dakom35@users.noreply.github.com>
2024-09-10 19:22:34 +0200

    - made a more modular testing method enabling me to get blocking & passing use cases - added test_output method to make output of results easier

--- src/UnitTesting/Entities/main.cpp
+++ src/UnitTesting/Entities/main.cpp
@@ -7,6 +7,9 @@
 
 bool testSufferingLongEnough(Entity entity);
 bool testSufferingNotLongEnough(Entity entity);
+bool isEntitySufferingAfterTime(Entity entity, float time_to_suffer);
+void test_output(int test_num, bool actual_result, bool expected_result);
+
 
 int main(void){
     std::cout << "Hello World ! " << "\n";
@@ -16,49 +19,50 @@ int main(void){
     std::cout << "Sprite obtained ! \n" ;
     //static_assert()
 
-    if(testSufferingLongEnough(entity) == true) std::cout << "Test #1 SUCCEDED !\n";
-    else{std::cout << "Test #1 FAILED ! \n";}
-    if(testSufferingNotLongEnough(entity)) std::cout << "Test #2 SUCCEDED !\n";
-    else{std::cout << "Test #2 FAILED ! \n";}
+    // TESTS I EXCPECT TO RETURN FALSE 
+    bool actual_result ;
+    actual_result = isEntitySufferingAfterTime(entity,2.f);
+    test_output(1,actual_result,false);
+    actual_result = isEntitySufferingAfterTime(entity,0.6f);
+    test_output(1,actual_result,false);
+    actual_result = isEntitySufferingAfterTime(entity,1.f);
+    test_output(1,actual_result,false);
 
+    // TESTS I EXPECT TO RETURN TRUE
+    actual_result = isEntitySufferingAfterTime(entity,0.4f);
+    test_output(2,actual_result,true);
+    actual_result = isEntitySufferingAfterTime(entity,0.3f);
+    test_output(2,actual_result,true);
+    actual_result = isEntitySufferingAfterTime(entity,0.3f);
+    test_output(2,actual_result,true);
 
 
     return 0 ; 
 
 }
 
-// Verify that manageSuffering disables suffering after 0.5s
-bool testSufferingLongEnough(Entity entity){
+// Verify that manageSuffering manages suffering correctly 
+// Entity should suffer for 0.5s not more not
+// Returns suffering attribute of entity
+// time_to_suffer (seconds)
+bool isEntitySufferingAfterTime(Entity entity, float time_to_suffer){
     sf::Clock clock ;
     sf::Time time ; 
-    entity.setSuffering(true); 
-    clock.restart(); // local clock starts before internal clock of manageSuffering()
-    while(entity.getSuffering()){
+    entity.manageSuffering(true); // reset internal clock to do like when Player is attacked twice during the 0.5s delay
+    entity.setSuffering(true);
+    clock.restart();
+    while(time.asSeconds() < time_to_suffer){
         time = clock.getElapsedTime();
-        entity.manageSuffering();
+        entity.manageSuffering(false);
     }
-    std::cout << "time(s)="<< time.asSeconds() << "\n"; // should be greater than 0.5s
-    if(time.asSeconds() >= 0.5) return true ; 
-    return false ;
+    return entity.getSuffering() ;    
 }
 
-bool testSufferingNotLongEnough(Entity entity){
-    // Verify that manageSuffering does not disable suffering before 0.5s
-    sf::Clock clock ;
-    sf::Time time ; 
-    entity.setSuffering(true);
-
-    clock.restart();
-    while(time.asSeconds() < 0.3f){
-        time = clock.getElapsedTime();
-        entity.manageSuffering();
+void test_output(int test_num, bool actual_result, bool expected_result){
+    if(actual_result == expected_result){
+        std::cout <<"TEST #" << test_num << " : SUCCESS :-) \n" ;
     }
-     
-    if(entity.getSuffering()){
-        // Entity should still suffer at this point because 0.5s not over
-        return true ;
+    else{
+        std::cout <<"TEST #" << test_num << " : FAIL :-( \n" ;
     }
-    
-    return false ;
-
 }
